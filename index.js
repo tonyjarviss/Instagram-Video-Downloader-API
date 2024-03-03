e041c43cf6435ff0b599c437c70aa0ffc2824aa0
tonyjarviss <157190551+tonyjarviss@users.noreply.github.com>
2024-03-03 17:41:07 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const snapsave = require('./snapsave-downloader');
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/get', (req, res) => {
   res.json({ message: 'Hello World!' });
 });
 
